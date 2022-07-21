3b0bd050ec8a2753fb14a1cf2290e80563c46bf8
Sophie Lai <syl416@cx3-4-29.cx3.hpc.ic.ac.uk>
2022-07-21 15:30:15 +0100

    upload 6_extract_sample_hebei R script, sh file, rds file

---
+++ Hebei/6_extract_sample_hebei.sh
@@ -0,0 +1,8 @@
+#PBS -l walltime=09:00:00
+#PBS -l select=1:ncpus=9:mem=500gb
+
+module load anaconda3/personal
+
+cd /rds/general/user/syl416/home/MScProject/Hebei
+
+Rscript extract_sample_hebei.R
