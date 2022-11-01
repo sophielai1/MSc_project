ce85248012eeddf771d6f99f49ab978b4c5ef5d9
Sophie Lai <syl416@cx3-5-29.cx3.hpc.ic.ac.uk>
2022-11-01 14:22:41 +0000

    tidying up files

--- Hebei/1_hebei_merge_tiles.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-setwd("/rds/general/user/syl416/home/MScProject/Hebei")
-
-hebei_tiles <- list.files(pattern="*.csv") %>%
-  lapply(read_csv) %>%
-  bind_rows
-
-saveRDS(hebei_tiles, file="hebei_tiles.rds")
--- Hebei/3_extract_gridID_hebei_2017_20.sh
+++
@@ -1,8 +0,0 @@
-#PBS -l walltime=12:00:00
-#PBS -l select=1:ncpus=9:mem=1200gb
-
-module load anaconda3/personal
-
-cd /rds/general/user/syl416/home/MScProject/Hebei
-
-Rscript extract_gridID_hebei_2017_20.R
